b1baae448daa3ce493dbda5e4d00bff2f558b948
Alexandre Pereira Santos <alex.endrios@hotmail.com>
2021-12-03 17:44:21 -0300

    regularization - merge

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -2,7 +2,6 @@ package pages;
 
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
-
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.sleep;
 import static org.junit.Assert.assertEquals;
